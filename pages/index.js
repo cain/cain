3692159d026ec5490a5da2afa1c8b491c8a79150
Cain Hall <cain.hall98@gmail.com>
2021-11-13 13:57:10 +1100

    homepage text

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,28 @@ export default function Home() {
       <main className={styles.main}>
         {/* <img src="/ch.svg" width="25px" class="mb-8" /> */}
         <div class=" mb-8">
-          {/* <h2 class="font-bold">Cain Hall</h2> */}
-          {/* <div className={styles.square}></div> */}
+          {/* <h1 class="font-bold">Cain Hall</h1> */}
+          {/* <a class="mr-2">writing</a>
+          <a class="mr-2">about</a>
+          <a class="mr-2">photos</a> */}
         </div>
-        <p class="mb-2">Hey,</p>
-        <p class="mb-2">
-          Im a frontend developer, <a href="https://www.plannthat.com" target="_blank">working at @plann</a>.
+        <p class="mb-2">Hey.</p>
+        <p class="mb-5">
+          I'm a frontend developer, <a href="https://www.plannthat.com" target="_blank">working at @plann</a>. We help build tools for people/businesses to connect and with their audience.
         </p>
-        <p class="font-semibold mb-5">My goal is to deliver a quality user experiences for amazing products I love.</p>
-        <div className={styles.contact}>
-          <a href="https://github.com/cain" target="_blank">Github</a>
-          <a href="https://www.linkedin.com/in/cainhall/" target="_blank">Linkedin</a>
-          <a href="mailto:me@cainhall.com.au" target="_blank">Email</a>
-          <a href="https://cain.github.io/resume/" target="_blank">Resume</a>
-        </div>
+        <h2 class="font-semibold mb-5">My goal is to deliver a quality user experience for amazing products I love.</h2>
+        {/* <div className={styles.contact}>
+          <a href="https://github.com/cain" target="_blank">github</a>
+          <a href="https://www.linkedin.com/in/cainhall/" target="_blank">linkedin</a>
+          <a href="mailto:me@cainhall.com.au" target="_blank">email</a>
+          <a href="https://cain.github.io/resume/" target="_blank">resume</a>
+        </div> */}
+        {/* <h3 class="font-bold mt-5 mb-2">Some Writings:</h3>
+        <ul class="list-disc pl-8">
+          <li><a>Nuxt 2 with eslint vs code formatting</a></li>
+          <li><a>Testing location date formatting with Chrome</a></li>
+          <li><a>Improving performance with VueJS components</a></li>
+        </ul> */}
       </main>
     </div>
   )
