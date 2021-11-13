c328fec7f6cf8ff147ccb38fd508ace0c1748c9e
Cain Hall <cain.hall98@gmail.com>
2021-11-13 15:42:29 +1100

    Homepage text updates

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        {/* <img src="/ch.svg" width="25px" class="mb-8" /> */}
-        <div class=" mb-8">
+        <img src="/ch.png" width="50px" class="m-auto" />
+        <div class="mb-8">
           {/* <h1 class="font-bold">Cain Hall</h1> */}
           {/* <a class="mr-2">writing</a>
           <a class="mr-2">about</a>
@@ -22,17 +22,21 @@ export default function Home() {
           I'm a frontend developer, <a href="https://www.plannthat.com" target="_blank">working at @plann</a>. We help build tools for people/businesses to connect and with their audience.
         </p>
         <h2 class="font-semibold mb-5">My goal is to deliver a quality user experience for amazing products I love.</h2>
-        {/* <div className={styles.contact}>
+        <div className={styles.contact}>
           <a href="https://github.com/cain" target="_blank">github</a>
           <a href="https://www.linkedin.com/in/cainhall/" target="_blank">linkedin</a>
           <a href="mailto:me@cainhall.com.au" target="_blank">email</a>
           <a href="https://cain.github.io/resume/" target="_blank">resume</a>
-        </div> */}
-        {/* <h3 class="font-bold mt-5 mb-2">Some Writings:</h3>
+        </div> 
+       {/* <h3 class=" mt-5 mb-2">Some Writings:</h3>
         <ul class="list-disc pl-8">
           <li><a>Nuxt 2 with eslint vs code formatting</a></li>
           <li><a>Testing location date formatting with Chrome</a></li>
           <li><a>Improving performance with VueJS components</a></li>
+        </ul>
+       <h3 class=" mt-5 mb-2">Things im working on:</h3>
+        <ul class="list-disc pl-8">
+          <li><a>Slack Strava Bot</a></li>
         </ul> */}
       </main>
     </div>
